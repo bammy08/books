5b68f8324660ac24df34d7a5280c49f0680a2ff4
bammy08 <olusolabamidele33@gmail.com>
2025-01-17 11:32:37 +0100

    removed vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This is the default output directory
+    // Other options you can configure based on your needs
+  },
+});
